f4561f6dfda4c3f57cfb1893d4b04ff184ce0a14
Eymar Lima <eymarlima@gmail.com>
2026-02-28 19:05:07 -0300

    fix: proxy

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import {NextRequest, NextResponse} from "next/server";
 
 const INTRANET_PUBLIC_PATHS = ["/login", "/reset-password", "/setup-password"];
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const {pathname} = request.nextUrl;
 
   // Protect yaaz pages (except the login page itself)
